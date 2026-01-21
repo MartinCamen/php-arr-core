ac81878716ad93b6d4e5ca0f1e0582de279ef7e5
Martin Camenius <camen@camensky.se>
2026-01-21 02:17:25 +0100

    Move functionality to enums

--- src/Enum/TrackedDownloadStatus.php
+++ src/Enum/TrackedDownloadStatus.php
@@ -13,11 +13,24 @@ enum TrackedDownloadStatus: string
 
     public static function fromSonarrToDownloadStatus(string $status): DownloadStatus
     {
-        return match (self::tryFrom(strtolower($status))) {
+        return match (self::make($status)) {
             self::Ok      => DownloadStatus::Downloading,
             self::Warning => DownloadStatus::Warning,
             self::Error   => DownloadStatus::Failed,
             default       => DownloadStatus::Unknown,
         };
+    }
+
+    public static function hasError(string $status): bool
+    {
+        return match (self::make($status)) {
+            self::Warning, self::Error => true,
+            default                    => false,
+        };
+    }
+
+    public static function make(string $status): ?self
+    {
+        return self::tryFrom(strtolower($status));
     }
 }
